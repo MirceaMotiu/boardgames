2005cb1dca55722dacd54e944bba8022b83ff379
Mircea Motiu <mircea.dan.motiu@gmail.com>
2019-04-15 20:13:18 +0300

    add parameter to template before render it

--- main/views.py
+++ main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html", {"message": "Hi there"})
+
