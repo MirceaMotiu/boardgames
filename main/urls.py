ca9c7ee0182470c500ba8240042f69a6f1f088d4
Mircea Motiu <mircea.dan.motiu@gmail.com>
2019-04-15 21:31:15 +0300

    add form to django

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import url
 from . import views
 
@@ -8,4 +8,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('formular/', views.formular, name="formular"),
+    path('formular_submit/', views.formular_submit, name="formular_submit"),
 ]
+
